936d9788d12863bf5931967daf6c37bcfa6fbf8d
Simranjyot Gill <ssg441@nyu.edu>
2017-10-15 18:19:50 -0400

    updates results : Evaluating on in-domain data:. Tag Accuracy: 0.9617146182848756 (Unknown Accuracy: 0.86302780638517)  Decoder Suboptimalities Detected: 0 Evaluating on out-of-domain data:. Tag Accuracy: 0.9307227606588726 (Unknown Accuracy: 0.7256129368805425)  Decoder Suboptimalities Detected: 0

--- hw3/script.zsh
+++ hw3/script.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-mv en-web-dev-in-domain.tagged output_in_domain_dev.txt
-mv en-web-dev-out-of-domain.tagged output_out_of_domain_dev.txt
-mv en-web-test.tagged output_test.txt
+cp /Users/simranjyot/nyu/stat-nlp/assignemnt-3/data/en-web-dev-in-domain.tagged output_in_domain_dev.txt
+cp /Users/simranjyot/nyu/stat-nlp/assignemnt-3/data/en-web-dev-out-of-domain.tagged output_out_of_domain_dev.txt
+cp /Users/simranjyot/nyu/stat-nlp/assignemnt-3/data/en-web-test.tagged output_test.txt
